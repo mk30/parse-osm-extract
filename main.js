4d6e7518a5ac9b2ffd0aee0624ed50e6bc0fa3bc
Marina Kukso <marina.kukso@gmail.com>
2019-01-15 19:35:21 -1000

    parser outputs mesh data

--- main.js
+++ main.js
@@ -1,17 +1,37 @@
-var fs = require('fs');
-var through = require('through2');
-var parseOSM = require('osm-pbf-parser');
+var fs = require('fs')
+var through = require('through2')
+var parseOSM = require('osm-pbf-parser')
+var earcut = require('earcut')
  
-var osm = parseOSM();
-var latLonArr = []
+var osm = parseOSM()
+var mesh = { positions: [], cells: [] }
+var nodes = {}
+
 fs.createReadStream(process.argv[2])
   .pipe(osm)
-  .pipe(through.obj(function (items, enc, next) {
-      items.forEach(function (item) {
-          latLonArr.push(item.lat + ', ' + item.lon)
-          //console.log(item.lat + ', ' +  item.lon);
-      });
-      next();
-      console.log(latLonArr)
-  }))
-;
+  .pipe(through.obj(write, end))
+
+function write (items, enc, next) {
+  items.forEach(function (item) {
+    if (item.type === 'node') {
+      nodes[item.id] = [item.lon, item.lat]
+    }
+    if (item.type === 'way') {
+      var polygon = []
+      var n = mesh.positions.length
+      item.refs.forEach(function (itemRef) {
+        polygon.push(nodes[itemRef][0], nodes[itemRef][1])
+        mesh.positions.push(nodes[itemRef])
+      })
+      var cells = earcut(polygon)
+      cells.forEach(function (cell) {
+        mesh.cells.push(cell + n)
+      })
+    }
+  })
+  next()
+}
+
+function end (next) {
+  console.log(JSON.stringify(mesh))
+}

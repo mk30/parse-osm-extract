31643ddc2a40ad2548b09a924eef0c9049a660ba
Marina Kukso <marina.kukso@gmail.com>
2019-01-19 12:25:19 -1000

    trianglestrip.js doubled norms are correct

--- trianglestrip.js
+++ trianglestrip.js
@@ -18,19 +18,29 @@ function write (items, enc, next) {
     if (item.type === 'node') {
       nodes[item.id] = [item.lon, item.lat]
     }
+    var waycoords = []
+    var waynorms = []
+    var waynormsdoubled = []
     if (item.type === 'way') {
-      prepositions.push(nodes[item.refs[0]])
+      waycoords.push(nodes[item.refs[0]])
       item.refs.forEach(function (itemRef) {
-        prepositions.push(nodes[itemRef])
-        prepositions.push(nodes[itemRef])
+        waycoords.push(nodes[itemRef])
       })
-      prepositions.push(nodes[item.refs[item.refs.length - 1]])
+      waycoords.push(nodes[item.refs[item.refs.length - 1]])
+      waynorms = getNormals(waycoords)
+      waynorms.forEach(function (norm) {
+        waynormsdoubled.push(norm[0])
+        waynormsdoubled.push([-1*norm[0][0], -1*norm[0][1]])
+      })
+      waynormsdoubled.push(waynormsdoubled[waynormsdoubled.length - 1])
+      console.log(waynormsdoubled)
     }
   })
   next()
 }
 
 function end (next) {
+/*
   prenormals = getNormals(prepositions)
   prepositions.forEach(function (pos) {
     mesh.positions.push(pos)
@@ -39,5 +49,6 @@ function end (next) {
     mesh.normals.push(norm[0])
     mesh.normals.push([-1*norm[0][0], -1*norm[0][1]])
   })
-  console.log(JSON.stringify(mesh))
+*/
+  //console.log(JSON.stringify(mesh))
 }
